d9243d8ee3e1dd42db354a27d02d1fdb166c8f20
maxskiier <m32285159@gmail.com>
2026-04-05 08:44:51 -0500

    Fixed a bug where the cursor would jump ahead on a backspace

--- c-code/video.h
+++ c-code/video.h
@@ -59,6 +59,8 @@ void terminal_setcolor(uint8_t color);
 
 void terminal_putentryat(char c, uint8_t color, size_t x, size_t y);
 
+void _put_backspace(uint8_t color, size_t x, size_t y);
+
 void terminal_putchar(char c);
 
 void terminal_write(const char* data, size_t size);
